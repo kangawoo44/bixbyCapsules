fb94d48b48a5e46624653294c77246c600c12321
kangawoo44 <wkang@viv.ai>
2020-07-15 16:06:41 -0700

    res.send

--- intercepter.js
+++ intercepter.js
@@ -8,7 +8,8 @@ function intercepter (req, res) {
     console.log(req.body)
     let jsonString = JSON.stringify(req.body, null, 2)
     fs.writeFileSync('./test.js', jsonString)
-    return null
+    return res.send('OK')
+    //res.sendStatus(200) === res.status(200).send('OK')
 }
 
 module.exports = router
