a2d187ffcb635bd65bc3d2aac2d92e64cda0e5d2
kangawoo44 <wkang@viv.ai>
2020-07-15 16:33:08 -0700

    edit

--- intercepter.js
+++ intercepter.js
@@ -5,8 +5,10 @@ const fs = require('fs')
 router.post('/', intercepter);
 
 function intercepter (req, res) {
-    console.log(req.body)
-    let jsonString = JSON.stringify(req.body, null, 2)
+    // console.log(req.body)
+    let items = req.body
+    // items = items.filter(x => x.hasSomething)
+    let jsonString = JSON.stringify(items, null, 2)
     fs.writeFileSync('./test.js', jsonString)
     return res.send('OK')
     //res.sendStatus(200) === res.status(200).send('OK')
