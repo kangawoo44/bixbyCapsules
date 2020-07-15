78c60914e110f07c5a27466fcebf489e4e9959d7
kangawoo44 <wkang@viv.ai>
2020-07-15 16:11:25 -0700

    change port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 90;
+const port = 9090;
 const bodyParser = require('body-parser');
 const intercepterRouter = require('./intercepter');
 
